4bf5ca48114533aa6d5e5f0de06aaea14c482bc5
Viktor Smari <viktorsmari@gmail.com>
2018-07-03 19:02:39 +0200

    Update correct url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site' /* title for your website */,
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
+  title: 'Smartcitizen documentation' /* title for your website */,
+  tagline: 'SmartCitizen',
+  url: 'https://fablabbcn.github.io/smartcitizen-docusaurus/' /* your website url */,
   baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'smartcitizen-docusaurus',
+  organizationName: 'fablabbcn',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -46,6 +46,11 @@ const siteConfig = {
     {blog: true, label: 'Blog'},
   ],
 
+  algolia: {
+    apiKey: 'my-api-key',
+    indexName: 'my-index-name',
+    algoliaOptions: {} // Optional, if provided by Algolia
+  },
   // If you have users set above, you add it here:
   users,
 
